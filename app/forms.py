91f59ac7dd1d646632bf89503b578f7750e84a66
notanewbiecoder <40542114+NotaNewbieCoder@users.noreply.github.com>
2020-05-30 19:22:05 +0100

    Add Login Feature

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Email', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
