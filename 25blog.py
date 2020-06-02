b0980f91206c335417d459f675e93bec7a146c25
notanewbiecoder <40542114+NotaNewbieCoder@users.noreply.github.com>
2020-06-02 21:45:49 +0100

    Add Database v2

--- 25blog.py
+++ 25blog.py
@@ -1,1 +1,7 @@
-from app import app
+from app import app, db
+from app.models import User, Post, Category, Comment, Profile
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post, 'Category': Category, 'Comment': Comment, 'Profile': Profile}
+    
--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    profile = db.relationship('Profile', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.name)
@@ -20,11 +21,10 @@ class Post(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
-    # category = db.relationship('Category', backref='post', lazy='dynamic')
     comments = db.relationship('Comment', backref='post', lazy='dynamic')
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        return '<Title {}, Post {}>'.format(self.title, self.body)
 
 class Newsletter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
